4a76df70c854a3b74c7f5814c3a96bb87c19352a
Ali8970 <aliabdelftah104@gmail.com>
2024-08-16 20:23:50 +0300

    handle isLoggedin to listen when user login

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   isLoggedIn = false; // This will be set to true after login
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {
+    this.authService.isLoggedIn.subscribe((status: boolean) => {
+      this.isLoggedIn = status;
+    });
+  }
 }
