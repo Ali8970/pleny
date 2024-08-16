3da8330ac0347aaaa47603680e8997929b335ad0
Ali8970 <aliabdelftah104@gmail.com>
2024-08-17 02:09:59 +0300

    create products component, get products and show it, search and filterByCategory, pagination

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { ProductService } from '../../view/products/product.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent implements OnInit {
+  @Output() searchQuery = new EventEmitter<string>();
+
   isLoggedIn = false; // This will be set to true after login
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private productService: ProductService
+  ) {}
   ngOnInit(): void {
     this.authService.isLoggedIn.subscribe((status: boolean) => {
       this.isLoggedIn = status;
     });
+  }
+  onSearchInput(event: KeyboardEvent): void {
+    const inputElement = event.target as HTMLInputElement;
+    if (event.key === 'Enter') {
+      this.productService.updateSearchTerm(inputElement.value);
+    }
   }
 }
