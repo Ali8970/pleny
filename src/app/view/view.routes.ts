cf5426fd8abbf1a0b460665e0020cd80ecdc4255
Ali8970 <aliabdelftah104@gmail.com>
2024-08-17 16:38:18 +0300

    create cart component and add routes

--- src/app/view/view.routes.ts
+++ src/app/view/view.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
+import { authGuard } from '../auth/auth.guard';
+import { CartComponent } from './cart/cart.component';
 
 export const routes: Routes = [
-  { path: 'products', component: ProductsComponent },
+  { path: 'products', canActivate: [authGuard], component: ProductsComponent },
+  { path: 'cart', canActivate: [authGuard], component: CartComponent },
 ];
